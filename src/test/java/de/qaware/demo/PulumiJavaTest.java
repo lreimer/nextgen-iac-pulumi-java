e1630329501a77fc5bbd5c24ebf36f40680d3f75
M.-Leander Reimer <mario-leander.reimer@qaware.de>
2024-10-27 13:41:11 +0100

    Added JUnit tests.

---
+++ src/test/java/de/qaware/demo/PulumiJavaTest.java
@@ -0,0 +1,45 @@
+package de.qaware.demo;
+
+import com.pulumi.test.Mocks;
+import com.pulumi.test.PulumiTest;
+import com.pulumi.gcp.storage.Bucket;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+
+import static com.pulumi.test.PulumiTest.extractValue;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+
+class PulumiJavaTest {
+
+    @AfterAll
+    static void cleanup() {
+        PulumiTest.cleanup();
+    }
+
+    @Test
+    void testSetupStorageBucket() {
+        var result = PulumiTest
+                .withMocks(new PulumiJavaMocks())
+                .runTest(PulumiJava::setupStorageBucket);
+
+        var bucket = result.resources().stream()
+                .filter(r -> r instanceof Bucket).map(r -> (Bucket) r)
+                .findFirst();
+
+        assertThat(bucket).isPresent().hasValueSatisfying(b -> {
+            assertThat(extractValue(b.location())).isEqualTo("EU");
+        });
+    }
+
+    public static class PulumiJavaMocks implements Mocks {
+        @Override
+        public CompletableFuture<ResourceResult> newResourceAsync(ResourceArgs args) {
+            return CompletableFuture.completedFuture(
+                    ResourceResult.of(Optional.of(args.name), args.inputs));
+        }
+    }
+}
